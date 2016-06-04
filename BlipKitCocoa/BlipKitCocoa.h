2e163b4d3ddb278aabe5e19cdb2fbad30f289c51
detomon <simon@monoxid.ch>
2016-06-04 12:27:04 +0200

    Add framework target

---
+++ BlipKitCocoa/BlipKitCocoa.h
@@ -0,0 +1,27 @@
+//
+//  BlipKitCocoa.h
+//  BlipKitCocoa
+//
+//  Created by Simon Schoenenberger on 04.06.16.
+//  Copyright © 2016 monoxid. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BlipKitCocoa.
+FOUNDATION_EXPORT double BlipKitCocoaVersionNumber;
+
+//! Project version string for BlipKitCocoa.
+FOUNDATION_EXPORT const unsigned char BlipKitCocoaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BlipKitCocoa/PublicHeader.h>
+
+#import <BlipKitCocoa/BlipKit.h>
+#import <BlipKitCocoa/BKTK.h>
+#import <BlipKitCocoa/BKCAudioUnit.h>
+#import <BlipKitCocoa/BKCBase.h>
+#import <BlipKitCocoa/BKCContext.h>
+#import <BlipKitCocoa/BKCDivider.h>
+#import <BlipKitCocoa/BKCInstrument.h>
+#import <BlipKitCocoa/BKCSample.h>
+#import <BlipKitCocoa/BKCTrack.h>
